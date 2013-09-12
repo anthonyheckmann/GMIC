f32fe083535684723e114a69af2cebc357f4c830
ronounours <David.Tschumperle@ensicaen.fr>
2013-09-12 13:52:28 +0200

    -

--- zart/.ui/DialogAbout.h
+++
@@ -1,8 +0,0 @@
-/****************************************************************************
-** Form interface generated from reading ui file 'ui/DialogAbout.ui'
-**
-** Created: Thu Feb 23 17:04:18 2012
-**
-** WARNING! All changes made in this file will be lost!
-****************************************************************************/
-
