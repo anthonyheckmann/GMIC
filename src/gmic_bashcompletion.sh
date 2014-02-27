58a0ab885aae4d3500cff7331468f0d95e4f9183
ronounours <David.Tschumperle@ensicaen.fr>
2014-02-27 23:07:27 +0100

    -

--- src/gmic_bashcompletion.sh
+++ src/gmic_bashcompletion.sh
@@ -2395,7 +2395,7 @@ _gmic_def()
 		return 0
 		;;
 		"-inpaint" | "--inpaint")
-		COMPREPLY=( $(compgen -W "[mask] [mask],0,_fast_method [mask],_patch_size>=1,_lookup_size>=1,_lookup_factor>=0,_blend_size>=0,0<=_blend_threshold<=1,_blend_decay>=0,_blend_scales>=1,_is_blend_outer={0|1}") )
+		COMPREPLY=( $(compgen -W "[mask] [mask],0,_fast_method [mask],_patch_size>=1,_lookup_size>=1,_lookup_factor>=0,_lookup_increment!=0,_blend_size>=0,0<=_blend_threshold<=1,_blend_decay>=0,_blend_scales>=1,_is_blend_outer={0|1}") )
 		return 0
 		;;
 		"-inpaint_flow" | "--inpaint_flow")
